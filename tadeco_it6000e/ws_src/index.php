bbc51dec5a30c0a7233a9d5172a519ed283cab99
Ferdinand Tomale <beowulf1416@gmail.com>
2011-10-05 09:09:34 -0400

    added test code for web service. added post handler.

--- tadeco_it6000e/ws_src/index.php
+++ tadeco_it6000e/ws_src/index.php
@@ -4,6 +4,22 @@
 include("iplist.php");
 include("settings.php");
 include("classes/service.class.inc");
+include("classes/db.class.inc");
 
-$service = new RestService();
+$callbacks = array(
+	"POST"=>"do_post"
+);
+
+$service = new RestService($callbacks);
 $service->handle();
+
+function do_post($server_variables = array(), $args = array()){
+	$json = json_decode($args["log"]);
+	
+	print("<pre>".print_r($server_variables, true)."</pre>");
+	print("<pre>".print_r($args, true)."</pre>");
+	print("<pre>");
+	var_dump($json);
+	print("</pre>");
+	die("do_post");
+}
---
+++ tadeco_it6000e/ws_src/tests/test_01.php
@@ -0,0 +1,20 @@
+<?php
+$json = "{\"param1\":\"value1\",\"param2\":\"value2\"}";
+?>
+<DOCTYPE html>
+<html>
+<head>
+	<title>Test 01</title>
+</head>
+<body>
+	<div class="messages">
+	</div>
+	<form name="form" method="post" action="../index.php">
+		<fieldset>
+			<legend>test 01</legend>
+			<input id="hidden" name="log" type="hidden" value="<?php print(htmlentities($json));?>" />
+		</fieldset>
+		<input id="submit" name="submit" type="submit" value="Test" />
+	</form>
+</body>
+</html>
